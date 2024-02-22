c29a1e34d1ee65f3cc3902f228d1ffafd3e8982b
André Fernandes <fpf.andre@gmail.com>
2024-02-22 13:33:02 -0300

    add pgx incmplete tests

---
+++ tests/test_pgx.py
@@ -0,0 +1,12 @@
+import jplm
+
+
+def test_read_bikes():
+    reader = jplm.PGXReader()
+    image = reader.read("tests/pgx_files/bikes.pgx")
+    # test stuff
+
+def test_read_greek():
+    reader = jplm.PGXReader()
+    image = reader.read("tests/pgx_files/greek.pgx")
+    # test stuff
